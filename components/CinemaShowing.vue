9175d8079614524121ca6d2175649cece91c2496
b-runz <bjarke.runz@outlook.dk>
2025-12-15 12:17:05 +0100

    Fix time overflow

--- components/CinemaShowing.vue
+++ components/CinemaShowing.vue
@@ -70,11 +70,11 @@ const haveAllShowingsPassed = (dateStr: string, showings: any[]): boolean => {
         <div class="font-bold text-lg mb-2">{{ cinema.name }}</div>
         <div class="flex flex-wrap gap-4">
           <template v-for="(showing, date) in cinema.showing" :key="date">
-            <div v-if="!haveAllShowingsPassed(date, showing)" class="bg-gray-100 border border-gray-300 rounded p-3 min-w-fit">
+            <div v-if="!haveAllShowingsPassed(date, showing)" class="bg-gray-100 border border-gray-300 rounded p-3">
               <div class="font-bold mb-2">{{ date }}</div>
-              <div class="flex flex-wrap gap-2">
+              <div class="flex flex-col gap-1">
                 <template v-for="show in showing" :key="show.link">
-                  <a v-if="!hasShowingPassed(date, show.time)" :href="show.link" class="text-black visited:text-red-600 font-bold underline bg-white px-2 py-1 rounded text-sm whitespace-nowrap">{{ show.time }}</a>
+                  <a v-if="!hasShowingPassed(date, show.time)" :href="show.link" class="text-black visited:text-red-600 font-bold underline bg-white px-2 py-1 rounded text-sm w-fit">{{ show.time }}</a>
                 </template>
               </div>
             </div>
